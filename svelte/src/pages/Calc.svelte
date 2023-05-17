02d7c4ce1fbce6818378b13e8f0b06f082faed1b
xorvet <cypherx702@gmail.com>
2023-05-17 23:22:10 +0530

    calc ui

--- svelte/src/pages/Calc.svelte
+++ svelte/src/pages/Calc.svelte
@@ -1,3 +1,29 @@
-<main>
-  hello calc
-</main>
+<div class="calc">
+  <div class="calc-out">
+  </div>
+  <div class="btn-div">
+    <div class="btns-col">
+      <button class="btn">AC</button>
+      <button class="btn">+</button>
+      <button class="btn">-</button>
+      <button class="btn">*</button>
+    </div>
+    <div class="btns-col">
+      <div class="num-div">
+        {#each nums as num}
+          <button class="btn">{num}</button>
+        {/each}
+      </div>
+      <div class="side-div">
+        <button class="btn">/</button>
+        <button class="btn">mod</button>
+        <button class="btn">%</button>
+        <button class="btn">=</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+const nums = [...Array(10).keys()];
+</script>
