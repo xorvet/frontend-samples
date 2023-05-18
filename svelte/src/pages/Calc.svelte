71705b7453ac6f746b8861a264afd47fbf134f54
xorvet <cypherx702@gmail.com>
2023-05-18 13:37:03 +0530

    calc ui

--- svelte/src/pages/Calc.svelte
+++ svelte/src/pages/Calc.svelte
@@ -2,13 +2,14 @@
   <div class="calc-out">
   </div>
   <div class="btn-div">
+    <button class="btn text-red-400">AC</button>
     {#each btns as btn}
       <button class="btn">{btn}</button>
     {/each}
-    <button class="btn col-span-3">=</button>
+    <button class="btn col-span-2">=</button>
   </div>
 </div>
 
 <script>
-const btns = "AC,+,-,*,0,1,2,/,3,4,5,mod,6,7,8,%,9".split(",");
+const btns = "+,-,*,1,2,3,/,4,5,6,mod,7,8,9,%,0,.".split(",");
 </script>
