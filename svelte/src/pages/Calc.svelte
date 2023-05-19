57bac7aa4771d5aaa6f43f66e1bc546ca310867c
xorvet <cypherx702@gmail.com>
2023-05-19 15:47:58 +0530

    calc

--- svelte/src/pages/Calc.svelte
+++ svelte/src/pages/Calc.svelte
@@ -1,16 +1,31 @@
 <div class="calc">
   <div class="calc-out">
-  <p class="m-auto opacity-70">123456</p> 
+    {out}
   </div>
   <div class="btn-div">
-    <button class="btn text-red-400 active:text-red-900">AC</button>
+    <button class="btn text-red-400 active:text-red-900" on:click={() => inn("AC")}>AC</button>
     {#each btns as btn}
-      <button class="btn">{btn}</button>
+      <button class="btn" on:click={() => inn(btn)}>{btn}</button>
     {/each}
-    <button class="btn col-span-2">=</button>
+    <button class="btn col-span-2" on:click={() => inn("=")}>=</button>
   </div>
 </div>
 
-<script>
-const btns = "+,-,*,1,2,3,/,4,5,6,mod,7,8,9,%,0,.".split(",");
+<script lang="ts">
+let out = '';
+const btns = "+,-,*,1,2,3,/,4,5,6,(,7,8,9,),0,.".split(",");
+
+function inn(val) {
+  if (val === "AC") {
+    out = '';
+  } else if(val === "=") {
+    try {
+      out = eval(out);
+    } catch(_e) {
+      out = "Error";
+    }
+  } else {
+    out += val
+  }
+}
 </script>
